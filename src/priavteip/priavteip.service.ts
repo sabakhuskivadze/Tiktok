6df57a82a84a2cf783d87e8737c549ef45e119f0
sabakhuskivadze <130067710+sabakhuskivadze@users.noreply.github.com>
2024-09-08 18:12:08 +0400

    SdsdS

--- src/priavteip/priavteip.service.ts
+++ src/priavteip/priavteip.service.ts
@@ -1,23 +1,25 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import * as os from 'os';
-import { Priavteip } from './entities/priavteip.entity';
-import { Repository } from 'typeorm';
-import { exec } from 'child_process';
-import { promisify } from 'util';
-const execPromise = promisify(exec);
-@Injectable()
-export class PriavteipService {
-  constructor(
-    @InjectRepository(Priavteip)
-    private readonly repository: Repository<Priavteip>,
-  ) {}
+// import { Injectable } from '@nestjs/common';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import * as os from 'os';
+// import { Priavteip } from './entities/priavteip.entity';
+// import { Repository } from 'typeorm';
+// import { exec } from 'child_process';
+// import { promisify } from 'util';
 
-  getPublicIp(request: Request): string | null {
-    // Retrieve the public IP address from headers
-    const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+// import {request}
+// const execPromise = promisify(exec);
+// @Injectable()
+// export class PriavteipService {
+//   constructor(
+//     @InjectRepository(Priavteip)
+//     private readonly repository: Repository<Priavteip>,
+//   ) {}
+
+//   getPublicIp(request: Request): string | null {
+//     // Retrieve the public IP address from headers
+//     const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
     
-    // Convert to string and return
-    return typeof ip === 'string' ? ip.split(',')[0].trim() : null;
-  }
-}
+//     // Convert to string and return
+//     return typeof ip === 'string' ? ip.split(',')[0].trim() : null;
+//   }
+// }
