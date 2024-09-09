7c0bca70ccb1d67dfdd85972dcd674cdbf2da160
sabakhuskivadze <130067710+sabakhuskivadze@users.noreply.github.com>
2024-09-09 19:28:36 +0400

    dsdsad

--- src/loc/loc.controller.ts
+++ src/loc/loc.controller.ts
@@ -1,38 +1,36 @@
-import { Body, Controller, HttpCode, HttpException, HttpStatus, Post } from '@nestjs/common';
-import { LocationService } from './loc.service';
+// import { Body, Controller, HttpCode, HttpException, HttpStatus, Post } from '@nestjs/common';
+// @Controller('l')
+// export class LocationController {
+//   constructor(private readonly locationService: LocationService) {}
 
-@Controller('l')
-export class LocationController {
-  constructor(private readonly locationService: LocationService) {}
+//   @Post('/save')
+//   @HttpCode(HttpStatus.OK)
+//   async saveLocation(@Body() body: { latitude: number; longitude: number }) {
+//     const { latitude, longitude } = body;
 
-  @Post('/save')
-  @HttpCode(HttpStatus.OK)
-  async saveLocation(@Body() body: { latitude: number; longitude: number }) {
-    const { latitude, longitude } = body;
+//     if (typeof latitude !== 'number' || typeof longitude !== 'number') {
+//       throw new HttpException('Invalid data: latitude and longitude must be numbers', HttpStatus.BAD_REQUEST);
+//     }
 
-    if (typeof latitude !== 'number' || typeof longitude !== 'number') {
-      throw new HttpException('Invalid data: latitude and longitude must be numbers', HttpStatus.BAD_REQUEST);
-    }
+//     try {
+//       const address = await this.locationService.getAddress(latitude, longitude);
 
-    try {
-      const address = await this.locationService.getAddress(latitude, longitude);
-
-      if (address) {
-        const { street, city, formattedAddress } = address;
-        console.log(`Address: ${formattedAddress}`);
-        return {
-          address: formattedAddress,
-          street,
-          city,
-          latitude,
-          longitude
-        };
-      } else {
-        throw new HttpException('Location not found', HttpStatus.NOT_FOUND);
-      }
-    } catch (error) {
-      console.error('Error fetching location:', error.message || error);
-      throw new HttpException('Error fetching location', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
-}
+//       if (address) {
+//         const { street, city, formattedAddress } = address;
+//         console.log(`Address: ${formattedAddress}`);
+//         return {
+//           address: formattedAddress,
+//           street,
+//           city,
+//           latitude,
+//           longitude
+//         };
+//       } else {
+//         throw new HttpException('Location not found', HttpStatus.NOT_FOUND);
+//       }
+//     } catch (error) {
+//       console.error('Error fetching location:', error.message || error);
+//       throw new HttpException('Error fetching location', HttpStatus.INTERNAL_SERVER_ERROR);
+//     }
+//   }
+// }
--- src/priavteip/priavteip.controller.ts
+++ src/priavteip/priavteip.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get, Req } from '@nestjs/common';
-import { PriavteipService } from './priavteip.service';
-import { Request } from 'express';
-@Controller('network')
-export class PriavteipController {
-  constructor(private readonly networkService: PriavteipService) {}
-  @Get()
-  getIp(@Req() request: Request): string {
-    const ip = this.networkService.getPublicIp(request);
-    return ip ? `Your public IP address is ${ip}` : 'Public IP address not found';
-  }
-}
+// import { Controller, Get, Req } from '@nestjs/common';
+// import { Request } from 'express';
+// @Controller('network')
+// export class PriavteipController {
+//   constructor(private readonly networkService: PriavteipService) {}
+//   @Get()
+//   getIp(@Req() request: Request): string {
+//     const ip = this.networkService.getPublicIp(request);
+//     return ip ? `Your public IP address is ${ip}` : 'Public IP address not found';
+//   }
+// }
